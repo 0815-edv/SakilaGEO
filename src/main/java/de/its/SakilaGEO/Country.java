718658c1ef7a69eadf440bf33a4c133b7e2d8589
flwerner <flwerner>
2021-07-15 13:03:53 +0200

    Update Class

--- src/main/java/de/its/SakilaGEO/Country.java
+++ src/main/java/de/its/SakilaGEO/Country.java
@@ -4,16 +4,27 @@
  * and open the template in the editor.
  */
 package de.its.SakilaGEO;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 
 /**
  *
  * @author flori
  */
+@Entity
+@Table(name="country")
 public class Country {
-    private int country_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long country_id;
     private String name;
 
-    public Country(int country_id, String name) {
+    public Country(Long country_id, String name) {
         this.country_id = country_id;
         this.name = name;
     }
@@ -21,11 +32,11 @@ public class Country {
     public Country() {
     }
 
-    public int getCountry_id() {
+    public Long getCountry_id() {
         return country_id;
     }
 
-    public void setCountry_id(int country_id) {
+    public void setCountry_id(Long country_id) {
         this.country_id = country_id;
     }
 
