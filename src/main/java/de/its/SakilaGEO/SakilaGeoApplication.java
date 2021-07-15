564e78799618e950e600ce70521560e7c842d596
flwerner <flwerner>
2021-07-15 12:39:48 +0200

    First Initialation

---
+++ src/main/java/de/its/SakilaGEO/SakilaGeoApplication.java
@@ -0,0 +1,13 @@
+package de.its.SakilaGEO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaGeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/its/SakilaGEO/SakilaGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.its.SakilaGEO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaGeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
