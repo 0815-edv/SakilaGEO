011401e5881c719f633a3c1df9490c9abcd87455
flwerner <flwerner>
2021-07-20 08:07:10 +0200

    pull

---
+++ Frontend/WebFrontend/WebFrontend/Data/ApiCountry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebFrontend.Data
+{
+    public class ApiCountry
+    {
+        public HttpClient client = new HttpClient();
+
+        private List<Country> country = new List<Country>();
+
+        public List<Country> GetCountries() {
+            client.GetAsync
+            return null;
+        }
+
+    }
+}
