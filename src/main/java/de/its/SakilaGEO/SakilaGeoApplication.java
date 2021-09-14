3a75c6e2dc3a63cd57369dd9f2a1dd1da9463b18
Markus <48181660+BackInBash@users.noreply.github.com>
2021-09-14 15:11:15 +0200

    Added CORS "Support"

--- src/main/java/de/its/SakilaGEO/SakilaGeoApplication.java
+++ src/main/java/de/its/SakilaGEO/SakilaGeoApplication.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "SakilaGEO API", version = "2.0", description = "GEO Information"))
@@ -11,6 +14,16 @@ public class SakilaGeoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SakilaGeoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
